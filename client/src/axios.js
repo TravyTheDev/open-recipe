8002f2f381fac5adad802caff0474f1bab953bbe
unknown <traviskleckley1@gmail.com>
2022-08-08 08:43:43 +0900

    changed api url

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: "http://localhost:8000",
+    baseURL: "https://open-recipe-v1.herokuapp.com/",
 })
 
 export default instance
