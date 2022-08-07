79a1ff5ef35aabeb78dbdd36a48d9d922e1a1b94
unknown <traviskleckley1@gmail.com>
2022-08-08 08:43:43 +0900

    changed api url

--- client/src/Api/api.js
+++ client/src/Api/api.js
@@ -2,7 +2,7 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
 export const api = createApi({
     reducerPath: 'api',
-    baseQuery: fetchBaseQuery({baseUrl: "http://localhost:8000"}),
+    baseQuery: fetchBaseQuery({baseUrl: "https://open-recipe-v1.herokuapp.com/"}),
     endpoints: (builder) => ({
         signup: builder.mutation({
             query: (user) => ({
