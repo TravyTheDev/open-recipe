92edbfa82a19eee936350982e1997ca7b8e5c27d
unknown <traviskleckley1@gmail.com>
2022-08-08 09:35:24 +0900

    took ../ out of path.join in server

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ app.use('/images', imageRoutes)
 
 //serve static assets
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join('../client/build')))
+    app.use(express.static(path.join('client/build')))
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html'))
