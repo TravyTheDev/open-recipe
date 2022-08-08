39d4a7d1c9733bd7961f67bafd5eba4e18c7a325
unknown <traviskleckley1@gmail.com>
2022-08-08 09:18:16 +0900

    took away ../ from server

--- server/server.js
+++ server/server.js
@@ -34,6 +34,6 @@ if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join('../client/build')))
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html'))
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     })
 }
